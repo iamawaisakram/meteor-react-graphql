2b8d71e0def06b6d5f8f9e5d2359307da3023b73
Awaisi <awais@amrood.de>
2019-05-27 07:24:02 +0500

    Adding Collections and fetching data from database

--- imports/api/resolutions/resolvers.js
+++ imports/api/resolutions/resolvers.js
@@ -3,16 +3,7 @@ import Resolutions from './resolutions';
 export default {
   Query: {
     resolutions() {
-      return [
-        {
-          _id: 'adsfadadfsadsf',
-          name: 'Get Stuff Done'
-        },
-        {
-          _id: 'ffffffffff',
-          name: 'Loose some weight'
-        }
-      ];
+      return Resolutions.find({}).fetch();
     }
   }
 };
